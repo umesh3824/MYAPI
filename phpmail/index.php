918f10bbf4c7d5610e534764b67af5e415ca223c
umeshchaudhari2001 <umeshchaudhari3824@gmail.com>
2021-11-30 18:08:33 +0530

    use just SMTP with Gmail

---
+++ phpmail/index.php
@@ -0,0 +1,54 @@
+<?php
+/*##########Script Information#########
+  # Purpose: Send mail Using PHPMailer#
+  #          & Gmail SMTP Server 	  #
+  # Created: 24-11-2019 			  #
+  #	Author : Hafiz Haider			  #
+  # Version: 1.0					  #
+  # Website: www.BroExperts.com 	  #
+  #####################################*/
+
+//Include required PHPMailer files
+	require 'includes/PHPMailer.php';
+	require 'includes/SMTP.php';
+	require 'includes/Exception.php';
+//Define name spaces
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\SMTP;
+	use PHPMailer\PHPMailer\Exception;
+//Create instance of PHPMailer
+	$mail = new PHPMailer();
+//Set mailer to use smtp
+	$mail->isSMTP();
+//Define smtp host
+	$mail->Host = "smtp.gmail.com";
+//Enable smtp authentication
+	$mail->SMTPAuth = true;
+//Set smtp encryption type (ssl/tls)
+	$mail->SMTPSecure = "tls";
+//Port to connect smtp
+	$mail->Port = "587";
+//Set gmail username
+	$mail->Username = "asstyu9@gmail.com";
+//Set gmail password
+	$mail->Password = "8805302569";
+//Email subject
+	$mail->Subject = "Test email using PHPMailer";
+//Set sender email
+	$mail->setFrom('asstyu9@gmail.com');
+//Enable HTML
+	$mail->isHTML(true);
+//Attachment
+	$mail->addAttachment('img/attachment.png');
+//Email body
+	$mail->Body = "<h1>This mail is form Umesh Chaudhari</h1><h1>It is just example of SMTP with Google Mail.</h1><h1>If you got mail properly just acknowledge me.</h1>";
+//Add recipient
+	$mail->addAddress('bhaveshpatil0404@gmail.com');
+//Finally send email
+	if ( $mail->send() ) {
+		echo "Email Sent..!";
+	}else{
+		echo "Message could not be sent. Mailer Error: "{$mail->ErrorInfo};
+	}
+//Closing smtp connection
+	$mail->smtpClose();
