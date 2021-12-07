f8295ad38d97489e06e712c95d6051cd8d6fb699
umeshchaudhari2001 <umeshchaudhari3824@gmail.com>
2021-12-07 09:48:07 +0530

    CSV module added

---
+++ CSV/csv.php
@@ -0,0 +1,26 @@
+<?php
+
+class CSV{
+    public $fileObj;
+    function __construct($filename){
+        $this->fileObj=fopen($filename, 'w+');
+    }
+    function setHeader($headerData){
+        fputcsv($this->fileObj, $headerData);
+    }
+    function setData($data){
+        fputcsv($this->fileObj, $data);
+    }
+    function close(){
+       fclose($this->fileObj);
+    }
+}
+
+
+// $CSVObj=new CSV('dummy.csv');
+// $CSVObj->setHeader(['name','mobile no']);
+// $data = ['FB','585955'];
+// $CSVObj->setData($data);
+
+
+?>
